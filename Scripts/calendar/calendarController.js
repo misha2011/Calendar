d5c5157dac0a6755ac838daa0eac07dd886f0262
Misha Shmyhelskyy <shmyhelskyy.misha@gmail.com>
2017-03-26 23:43:10 +0300

    finished

--- Scripts/calendar/calendarController.js
+++ Scripts/calendar/calendarController.js
@@ -4,51 +4,60 @@
         var ctrl = this;
         ctrl.day = moment();
         ctrl.newDate = new Date(ctrl.day);
+        ctrl.selected = "=";
         ctrl._removeTime = function (date) {            
-                return date.day(0).hour(0).minute(0).second(0).millisecond(0);
-                }
+            return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+        }
         ctrl._buildMonth = function (start, month) {
-                ctrl.weeks = [];
-                var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
-                while (!done) {
-                    ctrl.weeks.push({ days: ctrl._buildWeek(date.clone(), ctrl.month) });
-                    date.add(1, "w");
-                    done = count++ > 2 && monthIndex !== date.month();
-                    monthIndex = date.month();
-                    }        
-                }            
+            ctrl.weeks = [];
+            var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
+            while (!done) {
+                ctrl.weeks.push({ days: ctrl._buildWeek(date.clone(), ctrl.month) });
+                date.add(1, "w");
+                done = count++ > 2 && monthIndex !== date.month();
+                monthIndex = date.month();
+                }        
+        }            
         ctrl._buildWeek = function (date, month) {
-                    var days = [];
-                    for (var i = 0; i < 7; i++) {
-                        days.push({
-                            name: date.format("dd").substring(0, 1),
-                            number: date.date(),
-                            isCurrentMonth: date.month() === ctrl.month.month(),
-                            isToday: date.isSame(new Date(), "day"),
-                            date: date
-                        });
-                        date = date.clone();
-                        date.add(1, "d");
-                    }
-                    return days;
-                }   
-        ctrl.changeDate = function(){            
-            var dt = moment();
-            ctrl.day._d = ctrl.newDate;
-            ctrl.selected = ctrl._removeTime (ctrl.day);
-            ctrl.month = ctrl.selected.clone();       
-            var start = ctrl.selected.clone();       
-            start.date(1);
-            ctrl._removeTime(start.day(0));        
-            ctrl._buildMonth(start, ctrl.month);
-        };                 
-        ctrl.selected = ctrl._removeTime (moment());       
-        ctrl.month = ctrl.selected.clone();       
-        var start = ctrl.selected.clone();       
+            var days = [];
+            for (var i = 0; i < 7; i++) {
+                days.push({
+                    name: date.format("dd").substring(0, 1),
+                    number: date.date(),
+                    isCurrentMonth: date.month() === ctrl.month.month(),
+                    isToday: date.isSame(new Date(), "day"),
+                    date: date
+                });
+                date = date.clone();
+                date.add(1, "d");
+            }
+            return days;
+        }        
+        ctrl.changeDate = function(){ 
+             if( ctrl.newDate == undefined){
+                alert("Enter the correct date.");
+            }else{
+                ctrl.day._d = ctrl.newDate;
+                 ctrl.selectDate(ctrl.day);
+            }            
+        };
+        ctrl.today = function(){
+            ctrl.month = moment(); 
+             ctrl.selectDate(moment());
+        }
+        ctrl.selectDate = function(day){
+                ctrl.selected = ctrl._removeTime (day);
+                ctrl.month = day;       
+                var start = ctrl.selected.clone();      
+                start.date(1);
+                ctrl._removeTime(start.day(0));        
+                ctrl._buildMonth(start, ctrl.month);
+        }
+        ctrl.month = moment();       
+        var start = ctrl._removeTime (moment());         
         start.date(1);
         ctrl._removeTime(start.day(0));        
-        ctrl._buildMonth(start, ctrl.month);       
-        console.log(ctrl.weeks[0].days[0].date.format("MMMM"));
+        ctrl._buildMonth(start, ctrl.month); 
         ///modal window selected date
          ctrl.openModal = function (date) {
                 var ModalInstance = $uibModal.open({
